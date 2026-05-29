22d8aac5d63c0d2601eb0feb10e68e937c39abc1
Sounodip Ghosh <sounodip573@gmail.com>
2026-05-29 22:03:58 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       String newS=s.replaceAll("[^a-zA-Z0-9]","").strip().toLowerCase();
+        int left=0,right=newS.length()-1;
+        while(left<right){
+            if(newS.charAt(left)!=newS.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
